08004008e1a882481bdd780901b4d5208b0a2980
Emily <emily@bunni.me>
2024-01-09 14:13:42 -0500

    Supabase

--- src/main/kotlin/studio/pinkcloud/voyager/supabase/VoyagerSupabase.kt
+++ src/main/kotlin/studio/pinkcloud/voyager/supabase/VoyagerSupabase.kt
@@ -10,8 +10,8 @@ import io.netty.handler.codec.AsciiHeadersEncoder.NewlineType
 
 fun Application.createVoyagerSupabaseClient() {
     val supabase = createSupabaseClient(
-        "https://<your-project>.supabase.co",
-        "<your-anon-key>"
+        "https://qyyxkkbneewcycarhrey.supabase.co",
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5eXhra2JuZWV3Y3ljYXJocmV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ0ODU1MjAsImV4cCI6MjAyMDA2MTUyMH0.E8Mo1D9bsdBj6GWoRuRn5k9rBhKUBExO-LBWk_SQSTA"
     ) {
         // Configuration for the supabase client.
     }
