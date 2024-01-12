a4c77b97e784a20878ccf318966c77a56b693ae3
Nopox <151496498+nateweisz@users.noreply.github.com>
2024-01-11 23:55:47 -0800

    Update AbstractDeploymentSystem.kt

--- src/main/kotlin/studio/pinkcloud/voyager/deployment/AbstractDeploymentSystem.kt
+++ src/main/kotlin/studio/pinkcloud/voyager/deployment/AbstractDeploymentSystem.kt
@@ -72,7 +72,7 @@ abstract class AbstractDeploymentSystem(val prefix: String) {
                 port,
                 containerId,
                 cloudflareId,
-                false, /// this will always deploy the thing as a preview.
+                prefix.contains("prod"),
                 DeploymentState.DEPLOYED,
             )
 
