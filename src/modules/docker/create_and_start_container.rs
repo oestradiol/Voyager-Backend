94c60c60ee54037271bab59dcd110aa2559fb72c
Elaina <17bestradiol@proton.me>
2024-02-13 22:22:07 -0300

    Added error handling for container creation + multi-threading

--- src/modules/docker/create_and_start_container.rs
+++ src/modules/docker/create_and_start_container.rs
@@ -4,15 +4,17 @@ use bollard::{
   container::{Config, CreateContainerOptions},
   service::{HostConfig, PortBinding},
 };
+use tracing::{event, Level};
+use crate::utils::runtime_helpers::RuntimeSpawnHandled;
 
-use super::DOCKER;
+use super::{DOCKER, DOCKER_RUNTIME};
 
 pub async fn create_and_start_container(
   name: String,
   port: u16,
   internal_port: u16,
   docker_image: String,
-) -> Result<String, bollard::errors::Error> {
+) -> Option<String> {
   let options = Some(CreateContainerOptions {
     name: name,
     platform: Some("linux/amd64".to_string()),
@@ -35,70 +37,16 @@ pub async fn create_and_start_container(
     ..Default::default()
   };
 
-  DOCKER
-    .create_container(options, config)
-    .await
-    .map(|res| res.id)
+  DOCKER_RUNTIME.spawn_handled("modules::docker::create_and_start_container", DOCKER
+    .create_container(options, config)).await
+    .map(|res| {
+      match res {
+        Ok(res) => Some(res.id),
+        Err(err) => {
+          event!(Level::ERROR, "Failed to create Docker container! Error: {}", err);
+          None
+        }
+      }
+    })
+    .and_then(|res| res)
 }
-
-//     suspend fun createAndStartContainer(
-//         name: String,
-//         port: Int,
-//         internalPort: Int,
-//         dockerImage: String
-//     ): Result<String> = coroutineScope {
-//         log("Creating and starting container with name: $name, port: $port, internal port: $internalPort, docker image: $dockerImage", LogType.INFO)
-//         val context = newSingleThreadContext("DockerThread-${dockerImage.hashCode()}")
-//
-//         val id: String
-//
-//         try {
-//             id = async(context) {
-//                 log("Building docker id and blocking this thread..", LogType.DEBUG)
-//                 val idIn =
-//                     dockerClient
-//                         .createContainerCmd(dockerImage)
-//                         .withName(name)
-//                         // expose these ports inside the container
-//                         .withExposedPorts(
-//                             ExposedPort.tcp(internalPort)
-//                         )
-//                         .withHostConfig(
-//                             HostConfig.newHostConfig()
-//                                 .withPortBindings(
-//                                     // map the ${internalPort} port inside the container to the ${port} port on the host
-//                                     PortBinding(
-//                                         Ports.Binding.bindPort(port),
-//                                         ExposedPort.tcp(internalPort)
-//                                     )
-//                                 )
-//                         )
-//                         .exec()
-//                         .id // the id of the container that was created. (this container is not running yet)
-//
-//                 log("Container built with id $idIn", LogType.DEBUG)
-//                 log("Starting container with id $idIn", LogType.DEBUG)
-//
-//                 dockerClient
-//                     .startContainerCmd(idIn)
-//                     .exec()
-//
-//                 log("Container with id $idIn started", LogType.DEBUG)
-//
-//                 return@async idIn
-//             }.await()
-//
-//         } catch (err: Exception) {
-//             log("Error while creating and starting container:", LogType.ERROR)
-//             log(err)
-//             context.close()
-//
-//             return@coroutineScope Result.failure(err)
-//         } finally {
-//             context.close()
-//         }
-//
-//         return@coroutineScope Result.success(id)
-//
-//     }
-
