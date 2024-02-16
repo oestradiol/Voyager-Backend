e0b9394e70354cbc1a821653559ac5b6beb5e595
atomoxetine <emilybertollo@outlook.com>
2024-02-16 09:18:55 -0300

    added restart_container

--- src/modules/docker/restart_container.rs
+++ src/modules/docker/restart_container.rs
@@ -1,23 +1,6 @@
+use super::DOCKER;
 
-//
-//     suspend fun restartContainer(containerId: String): Result<Unit> {
-//         log("Restarting container with id: $containerId", LogType.INFO)
-//         return withContext(mainContext) {
-//             try {
-//                 if (isContainerRunning(containerId).getOrThrow()) {
-//                     log("Stopping container with container id $containerId", LogType.DEBUG)
-//                     dockerClient.stopContainerCmd(containerId).exec()
-//                     log("Container with id $containerId stopped", LogType.DEBUG)
-//                 }
-//
-//                 log("Starting container with id $containerId", LogType.DEBUG)
-//                 dockerClient.startContainerCmd(containerId).exec()
-//                 log("Container with id $containerId started")
-//
-//                 return@withContext Result.success(Unit)
-//             } catch (err: Exception) {
-//                 log("Error restarting container with id $containerId: ${err.localizedMessage}", LogType.ERROR)
-//                 return@withContext Result.failure(err)
-//             }
-//         }
-//     }
+async fn restart_container(container_id: String) -> Result<(), bollard::errors::Error> {
+  DOCKER.restart_container(&container_id, None).await
+}
+
